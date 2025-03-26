b412a909fc799fbc16ddfa96243f48b95a965a0e
anusha v <anushaventrapragada93@gmail.com>
2025-03-25 22:49:35 -0500

    updated social images

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,29 @@ const bungee = Cormorant_Garamond({
 export const metadata: Metadata = {
   title: "ANUSHA VENTRAPRGADA",
   description: "I love crafting sleek web applications",
+  openGraph: {
+    title: 'ANUSHA VENTRAPRGADA',
+    description: 'I love crafting sleek web applications',
+    url: 'https://your-domain.com',
+    siteName: 'Anusha Ventrapragada',
+    images: [
+      {
+        url: '/og-image.png', // Make sure to add this image to your public folder
+        width: 1200,
+        height: 630,
+        alt: 'Anusha Ventrapragada - Web Developer'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ANUSHA VENTRAPRGADA',
+    description: 'I love crafting sleek web applications',
+    images: ['/og-image.png'],
+    creator: '@yourtwitterhandle'
+  },
 };
 
 export default function RootLayout({
